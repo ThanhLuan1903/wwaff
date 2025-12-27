67972207972c8e3c9477976a52f902d96f4da7d7
ThanhLuan1903 <tranthanhluan1903@gmail.com>
2025-12-27 09:44:53 +0700

    Add example config files for staging

---
+++ application/config/database.example.php
@@ -0,0 +1,20 @@
+<?php
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'CHANGE_ME';
+$db['default']['database'] = 'wwaff';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['port'] = 3306;
+$db['default']['dbprefix'] = 'cpalead_';
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
