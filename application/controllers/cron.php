cdf666f54a43b90cd55ea3d26ac69740ef63f8fc
ThanhLuan1903 <tranthanhluan1903@gmail.com>
2026-01-28 16:28:05 +0700

    fix the log

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -4,19 +4,12 @@ class Cron extends CI_Controller
 {
     public function minpay_emails()
     {
-        if (method_exists($this->input, 'is_cli_request') && !$this->input->is_cli_request()) {
-            show_404(); return;
-        }
+log_message('info', 'CRON_START ' . date('c'));
 
-        // tránh notice do dự án đang dùng HTTP_HOST/REMOTE_ADDR trong CLI
-        if (empty($_SERVER['HTTP_HOST'])) $_SERVER['HTTP_HOST'] = 'localhost';
-        if (empty($_SERVER['REMOTE_ADDR'])) $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+$this->load->library('MinpayCron');
+$this->minpaycron->run();
 
-        echo "CRON_START " . date('c') . PHP_EOL;
+log_message('info', 'CRON_END ' . date('c'));
 
-        $this->load->library('MinpayCron');
-        $this->minpaycron->run();
-
-        echo "CRON_END " . date('c') . PHP_EOL;
     }
 }
